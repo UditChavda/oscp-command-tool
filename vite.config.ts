07c13a5f3a434cfc5c1db4055f16136a61b7bc4b
Udit <uchavda125@gmail.com>
2025-04-09 01:04:20 +0530

    Configure React app for deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
+    base: "/oscp-command-tool/",
+    server: {
     host: "::",
     port: 8080,
   },
